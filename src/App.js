38abd043a7a7a47cced84597d1abda423212a0dc
Vanessa <vanessa.ferreira.dev@gmail.com>
2023-04-01 20:47:36 +0100

    Login redirect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router,Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Login from "./pages/Login/Login"
 import React from 'react';
