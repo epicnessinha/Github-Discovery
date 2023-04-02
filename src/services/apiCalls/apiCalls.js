06c9da57be6c0d2cbdc1db7047f1626d2c9dd207
Vanessa <vanessa.ferreira.dev@gmail.com>
2023-04-02 19:31:47 +0100

    Backup

--- src/services/apiCalls/apiCalls.js
+++ src/services/apiCalls/apiCalls.js
@@ -1,5 +1,4 @@
 
-
 const API_BASE_URL = 'https://api.github.com';
 
 export const popularRepos = async (topic, sort) => {
@@ -9,3 +8,7 @@ export const popularRepos = async (topic, sort) => {
   const data = await response.json();
   return data.items;
 };
+
+// Add error handling to check the response status and throw an error if the fetch request fails.
+// Wrapped the code in a try-catch block to handle any errors that might occur during fetching and parsing the response.
+
