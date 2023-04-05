12d839d9395929e38911e018d053e87819a495dd
Vanessa <vanessa.ferreira.dev@gmail.com>
2023-04-05 13:30:06 +0100

    Header add to MyAccount

--- src/pages/MyAccount/MyAccount.jsx
+++ src/pages/MyAccount/MyAccount.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { getUser } from "../../services/apiCalls";
 import EditForm from "../../components/EditForm/EditForm";
+import Header from "../../components/Header/Header"
 import "./MyAccount.css";
 
 const MyAccount = ({ user, setUser }) => {
@@ -19,21 +20,23 @@ const MyAccount = ({ user, setUser }) => {
   }, [user.id]);
 
   return (
-    <div className="container d-flex justify-content-center align-items-center vh-100">
-      <div className="col-lg-6 col-md-8 col-sm-10">
-        <h1 className="title">My Account</h1>
-        <EditForm
-          username={username}
-          setUsername={setUsername}
-          email={email}
-          setEmail={setEmail}
-          userId={user.id}
-          setUser={setUser}
-          setErrorMessage={setErrorMessage}
-        />
-        {errorMessage && <p>{errorMessage}</p>}
+    <>
+      <Header isLoggedIn={true} />
+      <div className="container d-flex justify-content-center align-items-center vh-100">
+        <div className="col-lg-6 col-md-8 col-sm-10">
+          <EditForm
+            username={username}
+            setUsername={setUsername}
+            email={email}
+            setEmail={setEmail}
+            userId={user.id}
+            setUser={setUser}
+            setErrorMessage={setErrorMessage}
+          />
+          {errorMessage && <p>{errorMessage}</p>}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
